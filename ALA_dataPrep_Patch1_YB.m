49b401f623de9c00bfab2d9b3f5eb2a27a0d541b
ybenazouz <78807166+ybenazouz@users.noreply.github.com>
2022-05-04 15:05:38 +0200

    See comment last time (no push). main_expCOMETparts is completely incorrect from the fitting on, fixing it now.

--- ALA_dataPrep_Patch1_YB.m
+++ ALA_dataPrep_Patch1_YB.m
@@ -58,8 +58,55 @@ for i = 2:1:6
 end
 
 %% Load data - 670 nm
+%% O2norm 
+
+P1M1_O2norm_670nm = [];
+P1M2_O2norm_670nm = [];
+P1M3_O2norm_670nm = [];
+P1M4_O2norm_670nm = [];
+P1M5_O2norm_670nm = [];
+P1M6_O2norm_670nm = [];
+P1M7_O2norm_670nm = [];
+P1M8_O2norm_670nm = [];
+for i = 2:1:6
+    P1M1_O2norm_670nm = [P1M1_O2norm_670nm, xlsread('S1_M1_0911_O2norm.xlsx', i, 'B2:B4001')];
+    P1M2_O2norm_670nm = [P1M2_O2norm_670nm, xlsread('S1_M2_1052_O2norm.xlsx', i, 'B2:B4001')];
+    P1M3_O2norm_670nm = [P1M3_O2norm_670nm, xlsread('S1_M3_1156_O2norm.xlsx', i, 'B2:B4001')];
+    P1M4_O2norm_670nm = [P1M4_O2norm_670nm, xlsread('S1_M4_1250_O2norm.xlsx', i, 'B2:B4001')];
+    P1M5_O2norm_670nm = [P1M5_O2norm_670nm, xlsread('S1_M5_1353_O2norm.xlsx', i, 'B2:B4001')];
+    P1M6_O2norm_670nm = [P1M6_O2norm_670nm, xlsread('S1_M6_1455_O2norm.xlsx', i, 'B2:B4001')];
+    P1M7_O2norm_670nm = [P1M7_O2norm_670nm, xlsread('S1_M7_1556_O2norm.xlsx', i, 'B2:B4001')];
+    P1M8_O2norm_670nm = [P1M8_O2norm_670nm, xlsread('S1_M8_1657_O2norm.xlsx', i, 'B2:B4001')];
+end
+
+%% O20
+P1M1_O20_670nm = [];
+P1M2_O20_670nm = [];
+P1M3_O20_670nm = [];
+P1M4_O20_670nm = [];
+P1M5_O20_670nm = [];
+P1M6_O20_670nm = [];
+P1M7_O20_670nm = [];
+P1M8_O20_670nm = [];
+P1M9_O20_670nm = [];
+for i = 2:1:6
+    P1M1_O20_670nm = [P1M1_O20_670nm, xlsread('S1_M1_0911_O20.xlsx', i, 'B2:B4001')]; 
+    P1M2_O20_670nm = [P1M3_O20_670nm, xlsread('S1_M2_1052_O20.xlsx', i, 'B2:B4001')];
+    P1M3_O20_670nm = [P1M4_O20_670nm, xlsread('S1_M3_1156_O20.xlsx', i, 'B2:B4001')];
+    P1M4_O20_670nm = [P1M5_O20_670nm, xlsread('S1_M4_1250_O20.xlsx', i, 'B2:B4001')];
+    P1M5_O20_670nm = [P1M6_O20_670nm, xlsread('S1_M5_1353_O20.xlsx', i, 'B2:B4001')];
+    P1M6_O20_670nm = [P1M7_O20_670nm, xlsread('S1_M6_1455_O20.xlsx', i, 'B2:B4001')];
+    P1M7_O20_670nm = [P1M8_O20_670nm, xlsread('S1_M7_1556_O20.xlsx', i, 'B2:B4001')];
+    P1M8_O20_670nm = [P1M9_O20_670nm, xlsread('S1_M8_1657_O20.xlsx', i, 'B2:B4001')];
+end 
 
 %% Finding median of maxima 
+% This maximum assumes that the second measurement is the maximum value for
+% eacht measurement (?). Can we not first correct the data and then use
+% function max afterwards? 
+
+%% O2norm 
+% 630nm  
 maxP1M1_O2norm_630nm = -(median(P1M1_O2norm_630nm(2,:)));
 maxP1M2_O2norm_630nm = -(median(P1M2_O2norm_630nm(2,:)));
 maxP1M3_O2norm_630nm = -(median(P1M3_O2norm_630nm(2,:)));
@@ -69,24 +116,49 @@ maxP1M6_O2norm_630nm = -(median(P1M6_O2norm_630nm(2,:)));
 maxP1M7_O2norm_630nm = -(median(P1M7_O2norm_630nm(2,:)));
 maxP1M8_O2norm_630nm = -(median(P1M8_O2norm_630nm(2,:)));
 
+% 670nm 
+maxP1M1_O2norm_670nm = -(median(P1M1_O2norm_670nm(2,:)));
+maxP1M2_O2norm_670nm = -(median(P1M2_O2norm_670nm(2,:)));
+maxP1M3_O2norm_670nm = -(median(P1M3_O2norm_670nm(2,:)));
+maxP1M4_O2norm_670nm = -(median(P1M4_O2norm_670nm(2,:)));
+maxP1M5_O2norm_670nm = -(median(P1M5_O2norm_670nm(2,:)));
+maxP1M6_O2norm_670nm = -(median(P1M6_O2norm_670nm(2,:)));
+maxP1M7_O2norm_670nm = -(median(P1M7_O2norm_670nm(2,:)));
+maxP1M8_O2norm_670nm = -(median(P1M8_O2norm_670nm(2,:)));
 
-% max_ALA_total = [Max_MM_Pleister5_3uur Max_MM_Pleister6_4uur Max_MM_Pleister7_5uur Max_MM_Pleister8_6uur Max_MM_Pleister9_7uur Max_MM_Pleister10_8uur Max_MM_Pleister11_9uur Max_MM_Pleister12_10uur Max_MM_Pleister1_11uur Max_MM_Pleister2_12uur Max_MM_Pleister3_13uur Max_MM_Pleister4_14uur];
-% time_axis = 3:1:14;
-% 
-% figure(1)
-% plot(time_axis, max_ALA_total)
-% xlim([3 14])
-% ylabel('amplitude')
-% xlabel('time after ALA application (hours)')
-% title('Amplitude of 630nm measurement NL for time after ALA application MM')
+%% O20 
 
-%% Plotting amplitudes of 360nm data 
+%% Plotting intensities of 630 and 670 nm data 
+% Create vector with all maximum intensities and a time axis
+%% O2norm 
+maxP1MALL_O2norm_630nm = [maxP1M1_O2norm_630nm maxP1M2_O2norm_630nm maxP1M3_O2norm_630nm maxP1M4_O2norm_630nm 
+    maxP1M5_O2norm_630nm  maxP1M6_O2norm_630nm maxP1M7_O2norm_630nm maxP1M8_O2norm_630nm ] ; %630nm vector
+maxP1MALL_O2norm_670nm = [maxP1M1_O2norm_670nm maxP1M2_O2norm_670nm maxP1M3_O2norm_670nm maxP1M4_O2norm_670nm 
+    maxP1M5_O2norm_670nm  maxP1M6_O2norm_670nm maxP1M7_O2norm_670nm maxP1M8_O2norm_670nm ] ; %670nm vector
 
-% Max_MM_Pleister4_14uur = -(median(MM_Pleister4_14uur(2,:)));
-% 
-% max_ALA_total = [Max_MM_Pleister5_3uur Max_MM_Pleister6_4uur Max_MM_Pleister7_5uur Max_MM_Pleister8_6uur Max_MM_Pleister9_7uur Max_MM_Pleister10_8uur Max_MM_Pleister11_9uur Max_MM_Pleister12_10uur Max_MM_Pleister1_11uur Max_MM_Pleister2_12uur Max_MM_Pleister3_13uur Max_MM_Pleister4_14uur];
-% time_axis = 3:1:14;
-% 
-% figure(1)
-% plot(time_axis, max_ALA_total)
-% xlim([3 14])
+t = 3:1:14 ; % time axis
+
+figure(1)
+subplot(2,1,1) % maximum intensities at 630nm at different measurements 
+plot(t, maxP1MALL_O2norm_630nm)
+xlim([3 14])
+ylabel('Intensity [V]')
+xlabel('Measurements after application')
+title('Intensity of fluorescence signal at 630nm in subject #3')
+
+subplot(2,1,2) % maximum intensities at 670nm at different measurements 
+plot(t, maxP1MALL_O2norm_670nm)
+xlim([3 14])
+ylabel('Intensity [V]')
+xlabel('Measurements after application')
+title('Intensity of fluorescence signal at 670nm in subject #3')
+
+figure(2) % 630nm and 670nm together at different measurements 
+plot(t, maxP1MALL_O2norm_630nm, t, maxP1MALL_O2norm_670nm)
+xlim([3 14])
+legend('630nm', '670nm') 
+ylabel('Intensity [V]')
+xlabel('Measurements after application')
+title('Intensity of fluorescence signal at 630nm and 670nm in subject #3')
+
+%% O20
