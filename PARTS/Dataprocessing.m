d9fb9deca68ca2abb8df55e70528cbe8de1dfa85
ybenazouz <78807166+ybenazouz@users.noreply.github.com>
2022-05-05 11:24:23 +0200

    Delete PARTS directory

--- PARTS/Dataprocessing.m
+++
@@ -1,9 +0,0 @@
-
-% We want to smooth out the data. 
-
-% Average all measurements
-mean = mean(x,2) ;
-
-% Correct mean 
-mean_correct = mean(mean_MM_BGS_total(end-4:end));
-mean_correct_MM_BGS = mean_MM_BGS_total - mean_correct;
