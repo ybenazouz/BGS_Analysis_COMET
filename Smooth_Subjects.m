337c41467c20d6406ea8c603d1f5e8a88a0f9823
M. Schoenmakers <schoenmakers.m.a@gmail.com>
2022-06-08 13:16:42 +0200

    Changed some text behind %..

--- Smooth_Subjects.m
+++ Smooth_Subjects.m
@@ -1,16 +1,16 @@
 function [SUBJECTS] = Smooth_Subjects(SUBJECTS)
 subs = fieldnames(SUBJECTS) ; 
-for ss = 1:numel(subs) %subjects
+for ss = 1:numel(subs) %number of subjects
     data = SUBJECTS.(subs{ss}) ; 
     fields = fieldnames(SUBJECTS.(subs{ss})) ;
-    for pp = 1:numel(fields) % patches 
-        data2 = data.(fields{pp}) ; 
+    for pp = 1:numel(fields) % Number of patches 
+        data2 = data.(fields{pp}) ; %data from 1 patch
         
-        MM = numel(data2(:,:,1)) ; % amount of measurements done per patch
+        MM = numel(data2(:,:,1)) ; % Number of measurements done per patch
         wave = cell(2,MM) ; % 670 and 630 nm togeher for every measurement
-        for ii = 1:MM % amount of measurements done for patch one, we will average every measurement
+        for ii = 1:MM % Number of measurements done for patch one, we will average every measurement
             data3 = data2(:,ii,5) ; % all data patch one measurement one 1x1 cell with 1x5 cell 
-            M = numel(data3{1,1}); % amount of measurements done per measurement % M = numel(dS1M1{1,patch number for different patches});
+            M = numel(data3{1,1}); % Number of measurements done per measurement % M = numel(dS1M1{1,patch number for different patches});
 
             wave630 = zeros(4000,M);
             wave670 = zeros(4000,M);
